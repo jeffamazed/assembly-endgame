794e94b91a3f0f14f61a9c2ceeea03b5e425781c
Jeffry <jeffry1408.job@gmail.com>
2025-05-16 07:14:37 +0700

    Add files via upload

---
+++ js/audio.js
@@ -0,0 +1,6 @@
+const sfxLib = {
+  winning: new Audio("./audio/winning-final.mp3"),
+  losing: new Audio("./audio/losing-final.mp3"),
+  click: new Audio("./audio/click-final.mp3"),
+  booo: new Audio("./audio/booo-final.mp3")
+};
